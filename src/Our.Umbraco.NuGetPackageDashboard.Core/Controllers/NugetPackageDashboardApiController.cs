163e246bb9d9a65ad664302cd849d8c9f8e37d2d
Jamie Pollock <jamie.pollock@connect-group.com>
2016-07-12 11:57:30 +0100

    Added logging & caching to Api Controller

--- src/Our.Umbraco.NuGetPackageDashboard.Core/Controllers/NugetPackageDashboardApiController.cs
+++ src/Our.Umbraco.NuGetPackageDashboard.Core/Controllers/NugetPackageDashboardApiController.cs
@@ -5,8 +5,11 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Hosting;
 using System.Web.Http;
+using Our.Umbraco.NuGetPackageDashboard.Core.Models;
 using Our.Umbraco.NuGetPackageDashboard.Core.Services;
 using Umbraco.Core;
+using Umbraco.Core.Cache;
+using Umbraco.Core.Logging;
 using Umbraco.Web.WebApi;
 using Umbraco.Web.WebApi.Filters;
 
@@ -21,19 +24,20 @@ namespace Our.Umbraco.NuGetPackageDashboard.Core.Controllers
             try
             {
                 var filePath = HostingEnvironment.MapPath("~/Packages.config");
-
                 if (File.Exists(filePath) == false)
                 {
-                    throw new FileNotFoundException("Unable to find Packages.config file.", filePath);
+                    throw new FileNotFoundException("Unable to find Packages file.", filePath);
                 }
-
                 var service = new XmlConfigFileNuGetPackageService();
-                var packages = service.GetPackages(filePath);
+                var packages = ApplicationContext.ApplicationCache.RuntimeCache.GetCacheItem<IEnumerable<NuGetPackage>>(
+                    PackageConstants.CacheKey, () => service.GetPackages(filePath));
 
+                LogHelper.Info(typeof(NuGetPackageDashboardApiController), "Added serialized Packages file (path: \"{0}\") into ApplicationCache.RuntimeCache (key: \"{1}\").", () => new[] { filePath, PackageConstants.CacheKey });
                 return Request.CreateResponse(packages);
             }
             catch (Exception ex)
             {
+                LogHelper.Error(typeof(NuGetPackageDashboardApiController), "Unable to process Get API call.", ex);
                 return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
             }
         }
